e92ee17fc6c8f2a0c455e8273bfbe60fbccf5bb9
acidtib <acidtib@protonmail.com>
2018-10-29 23:49:39 -0400

    weycoin changes

--- db.h
+++ db.h
@@ -16,7 +16,7 @@
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 2000 : 1452255;
+    return testnet ? 2000 : 10526;
 }
 
 std::string static inline ToString(const CService &ip) {
